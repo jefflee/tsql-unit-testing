be66168083b42bd12af701cf21270564a9e8e4db
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2023-01-28 20:45:38 +0800

    Verify error in unit test

---
+++ Code/SalesDB/SalesDB.Test/uspCancelOrdertest.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalesDB.Test
+{
+    [TestClass()]
+    public class uspCancelOrdertest : SqlDatabaseTestClass
+    {
+
+        public uspCancelOrdertest()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction Sales_uspCancelOrderTest_TestAction;
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.ScalarValueCondition scalarValueCondition1;
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction Sales_uspCancelOrderTest_PretestAction;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(uspCancelOrdertest));
+            this.Sales_uspCancelOrderTestData = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestActions();
+            Sales_uspCancelOrderTest_TestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            scalarValueCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.ScalarValueCondition();
+            Sales_uspCancelOrderTest_PretestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            // 
+            // Sales_uspCancelOrderTestData
+            // 
+            this.Sales_uspCancelOrderTestData.PosttestAction = null;
+            this.Sales_uspCancelOrderTestData.PretestAction = Sales_uspCancelOrderTest_PretestAction;
+            this.Sales_uspCancelOrderTestData.TestAction = Sales_uspCancelOrderTest_TestAction;
+            // 
+            // Sales_uspCancelOrderTest_TestAction
+            // 
+            Sales_uspCancelOrderTest_TestAction.Conditions.Add(scalarValueCondition1);
+            resources.ApplyResources(Sales_uspCancelOrderTest_TestAction, "Sales_uspCancelOrderTest_TestAction");
+            // 
+            // scalarValueCondition1
+            // 
+            scalarValueCondition1.ColumnNumber = 1;
+            scalarValueCondition1.Enabled = true;
+            scalarValueCondition1.ExpectedValue = "0";
+            scalarValueCondition1.Name = "scalarValueCondition1";
+            scalarValueCondition1.NullExpected = false;
+            scalarValueCondition1.ResultSet = 1;
+            scalarValueCondition1.RowNumber = 1;
+            // 
+            // Sales_uspCancelOrderTest_PretestAction
+            // 
+            resources.ApplyResources(Sales_uspCancelOrderTest_PretestAction, "Sales_uspCancelOrderTest_PretestAction");
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+        [TestMethod(), ExpectedSqlException(Severity = 16, MatchFirstError = false, State = 1)]
+        public void Sales_uspCancelOrderTest()
+        {
+            SqlDatabaseTestActions testActions = this.Sales_uspCancelOrderTestData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            try
+            {
+                // Execute the test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+                SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            }
+            finally
+            {
+                // Execute the post-test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+                SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+            }
+        }
+        private SqlDatabaseTestActions Sales_uspCancelOrderTestData;
+    }
+}
